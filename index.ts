89bda571240dc8818a7cfe4b6706e7833b2c28aa
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-02-16 23:34:11 +0530

    set main file for serverless

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 // Import required modules
-// import ServerlessHttp from "serverless-http";
+import ServerlessHttp from "serverless-http";
 import app from "./src/app";
-import config from "./src/config";
+// import config from "./src/config";
 
 // Start server
 // if (config.port === "development") {
-const PORT = config.port || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = config.port || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 // }
-// export const handler = ServerlessHttp(app);
+export const handler = ServerlessHttp(app);
